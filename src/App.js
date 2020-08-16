3bc2486e8e561979308fc99ce1d5fe60e1414044
weshouck32 <33504406+weshouck32@users.noreply.github.com>
2020-08-16 12:41:36 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
   const [page] = useState(1);
   const [count] = useState(1);
   const [data, setData] = useState([["Loading Data..."]]);
+  const [setIsLoading] = useState(false);
 
   const APP_ID = process.env.REACT_APP_NUTRIION_API_APP_ID;
   const APP_KEY = process.env.REACT_APP_NUTRIION_API_APP_KEY;
@@ -26,11 +27,9 @@ function App() {
           sort: true,
           customBodyRender: (value) => {
             return (
-            <div><img className="App-thumb" alt="" src={value} /></div>
+            <div><img className="App-thumb" src={value} alt='' /></div>
             );
           },
-
-
         }
       },
       { name: "brand_name", options: { filter: true, sort: true, } },
@@ -50,6 +49,7 @@ function App() {
     ];
 
   //const changePage = (page) => {
+//setIsLoading(true);
 //getData(page);
 
 //getData2(page);
@@ -82,27 +82,17 @@ function App() {
     console.log(action, tableState);
     // a developer could react to change on an action basis or
     // examine the state as a whole and do whatever they want
-
-      //switch (action) {
-      //  case "changePage":
-      //    changePage(tableState.page);
-      //    alert(page);
-      //    break;
-    //  }
+/*
+      switch (action) {
+        case "changePage":
+          changePage(tableState.page);
+          alert(page);
+          break;
+      }
+      */
     },
   };
 
-  /*
-  const options = {
-  filterType: 'checkbox',
-  serverSide: true,
-  onTableChange: (action, tableState) => {
-    this.xhrRequest('/getdata.js?id=1', result => {
-      this.setState({ data: result });
-    });
-  }
-};
-*/
 
 var rows = [];
 
@@ -110,27 +100,30 @@ function getNutirtionData(nix_item_id) {
   $.ajax({
     url: "https://trackapi.nutritionix.com/v2/search/item",
     type: "GET",
+    async: false,
     dataType: 'json',
     headers: { 'x-app-id': APP_ID,
                'x-app-key': APP_KEY
              },
     contentType: 'application/json; charset=utf-8',
-    data: { nix_item_id: nix_item_id },
+    data: { 'nix_item_id': nix_item_id },
     success: function(nutritiondata) {
-      var api_data = [nutritiondata.foods[0].photo.thumb, nutritiondata.foods[0].food_name, nutritiondata.foods[0].serving_qty, nutritiondata.foods[0].serving_unit,
-                      nutritiondata.foods[0].serving_weight_grams, nutritiondata.foods[0].nf_calories, nutritiondata.foods[0].nf_total_fat,
-                      nutritiondata.foods[0].nf_saturated_fat, nutritiondata.foods[0].nf_cholesterol, nutritiondata.foods[0].nf_sodium, nutritiondata.foods[0].nf_total_carbohydrate,
-                      nutritiondata.foods[0].nf_dietary_fiber, nutritiondata.foods[0].nf_sugars, nutritiondata.foods[0].nf_protein, nutritiondata.foods[0].nf_potassium];
-      rows.push(api_data);
-    }
+      var n_data = [nutritiondata.foods[0].photo.thumb, nutritiondata.foods[0].food_name, nutritiondata.foods[0].serving_qty, nutritiondata.foods[0].serving_unit,
+                        nutritiondata.foods[0].serving_weight_grams, nutritiondata.foods[0].nf_calories, nutritiondata.foods[0].nf_total_fat,
+                        nutritiondata.foods[0].nf_saturated_fat, nutritiondata.foods[0].nf_cholesterol, nutritiondata.foods[0].nf_sodium, nutritiondata.foods[0].nf_total_carbohydrate,
+                        nutritiondata.foods[0].nf_dietary_fiber, nutritiondata.foods[0].nf_sugars, nutritiondata.foods[0].nf_protein, nutritiondata.foods[0].nf_potassium];
+      rows.push(n_data);
+  },
   });
-
 }
 
-const nutritionSearch = (page = 1) => {
+var nix = [];
+
+function getNutritionIDs() {
   $.ajax({
     url: "https://trackapi.nutritionix.com/v2/search/instant",
     type: "GET",
+    async: false,
     dataType: 'json',
     headers: { 'x-app-id': APP_ID,
                'x-app-key': APP_KEY
@@ -138,26 +131,31 @@ const nutritionSearch = (page = 1) => {
     contentType: 'application/json; charset=utf-8',
     data: { query: $("#searchbox").val() },
     success: function(result) {
-      rows = [];
       for (var i = 0; i < result.branded.length; i++) {
-        getNutirtionData(result.branded[i].nix_item_id);
-        //console.log(nutrition_data);
-        //var api_data = [result.branded[i].photo.thumb, result.branded[i].brand_name_item_name, result.branded[i].serving_qty, result.branded[i].serving_unit, "serving_weight_grams", result.branded[i].nf_calories, "nf_total_fat",
-        //                  "nf_saturated_fat", "nf_cholesterol", "nf_sodium", "nf_total_carbohydrate", "nf_dietary_fiber",
-        //                  "nf_sugars", "nf_protein", "nf_potassium"];
-        //rows.push(api_data);
+      //for (var i = 0; i < 3; i++) {
+        nix.push(result.branded[i].nix_item_id);
       }
-      setData(rows);
     }
   });
- }
+}
+
+const nutritionSearch = (page = 1) => {
+  //setIsLoading(true);
+  getNutritionIDs();
+
+  for (var i = 0; i < nix.length; i++) {
+    var nutritiondata = getNutirtionData(nix[i]);
+  }
+  setData(rows);
+
+}
 
   return (
     <MuiThemeProvider >
     <div className="App">
       <h1>Nutrition Information App</h1>
       <div className="row app-text-left">
-        <input id="searchbox" type="text"></input>
+        <input id="searchbox" type="text" value="yogurt"></input>
           <Button onClick={nutritionSearch}>Search</Button>
       </div>
       <MUIDataTable title={"Nutrition Information"} data={data} columns={columns} options={options} />
